03bd22b100d6ea61cae91b14d6e3a0616b31de12
veselyo <112834371+veselyo@users.noreply.github.com>
2022-12-02 17:32:17 -0500

    Add files via upload

--- helpers.py
+++ helpers.py
@@ -14,6 +14,7 @@ our_leagues = {
 
 # No need to run this function in app.py! We already called this function and gathered the data we needed and pasted it in SQL database
 # To call this function, import get_leagues_stats and our_leagues from helpers into app py and paste appropriate arguments
+# Only need to call this function when we change our desired leagues
 def get_leagues_stats(leagues, connection, database):
 
     # API documentation for getitng a response for active leagues
@@ -31,6 +32,10 @@ def get_leagues_stats(leagues, connection, database):
     if not response:
         print("Couldn't connect to API")
         return False
+
+    # Delete existing data
+    database.execute("DELETE FROM leagues")
+    connection.commit()
 
     # Insert information in a SQL database
     response = response["response"]
@@ -56,39 +61,58 @@ def get_standings(league_id, season_year):
     response = requests.request("GET", url, headers=headers, data=payload).json()
     return response["response"][0]["league"]["standings"][0]
 
-def make_standings(database):
-    # List of league ids we are going to work with, sorted by id
-    leagues = database.execute("SELECT id FROM leagues ORDER BY id")
-    leagues = [x[0] for x in leagues.fetchall()]
+def make_standings(league_id, season_year):
 
-    # Declaring list of list of dictionaries where we are going to store our desired data (easiest way to loop through them in flask afterwards)
-    standings = []
+    # Temporary variable where we store API's output
+    tmp = get_standings(league_id, season_year)
+    if not tmp:
+        print("Couldn't get standings")
+        sys.exit(1)
 
-    # Loop through the sorted ids and get standings for each league
-    for id in leagues:
-        tmp = get_standings(id, 2022)
-        if not tmp:
-            print("Couldn't get standings")
-            sys.exit(1)
+    # Variable to store list of dicionaries (teams) that is going to be returned
+    listofteams = []
 
-        # temporary variable to store list of dicionaries of teams
-        listofteams = []
+    # Store key value pairs that we want to display on our website
+    for item in tmp:
+        # Temporary dictionary
+        teams = {}
+        teams.update({"rank" : item["rank"]})
+        teams.update({"name" : item["team"]["name"]})
+        teams.update({"logo" : item["team"]["logo"]})
+        teams.update({"points" : item["points"]})
+        teams.update({"played" : item["all"]["played"]})
+        teams.update({"win" : item["all"]["win"]})
+        teams.update({"draw" : item["all"]["draw"]})
+        teams.update({"lose" : item["all"]["lose"]})
+        teams.update({"goalsfor" : item["all"]["goals"]["for"]})
+        teams.update({"goalsagainst" : item["all"]["goals"]["against"]})
 
-        # Store key value pairs that we want to display on our website
-        for item in tmp:
-            teams = {}
-            teams.update({"rank" : item["rank"]})
-            teams.update({"name" : item["team"]["name"]})
-            teams.update({"logo" : item["team"]["logo"]})
-            teams.update({"points" : item["points"]})
-            teams.update({"played" : item["all"]["played"]})
-            teams.update({"win" : item["all"]["win"]})
-            teams.update({"draw" : item["all"]["draw"]})
-            teams.update({"lose" : item["all"]["lose"]})
-            teams.update({"goalsfor" : item["all"]["goals"]["for"]})
-            teams.update({"goalsagainst" : item["all"]["goals"]["against"]})
-            listofteams.append(teams)
-        # At the end, append each list of teams to standings, creating list of list of dictionaries
-        standings.append(listofteams)
-    return standings
+        # Append the temp dictionary to our list
+        listofteams.append(teams)
+
+    # Return list of teams (dictionaries)
+    return listofteams
+
+# Convert league name input to an id
+def convertinput(database, league_name):
+    id = database.execute("SELECT id FROM leagues WHERE name = ?", [league_name])
+    id = id.fetchone()
+    id = id[0]
+    return id
+
+# Get the name of either last or next round of a league depending on the input, we will use this to display it on the website and access data
+def get_next_or_last_round(league_id, season_year, next_or_last):
+    # API documentation for getitng a response for standings
+    url = f"https://v3.football.api-sports.io/fixtures?season={season_year}&league={league_id}&{next_or_last}=1"
+
+    payload={}
+    headers = {
+        'x-rapidapi-key': 'c4b2f02de36f4916cc87ab129e628422',
+        'x-rapidapi-host': 'v3.football.api-sports.io'
+    }
+
+    # returning the response we want in a json format
+    response = requests.request("GET", url, headers=headers, data=payload).json()
+    response = response["response"][0]["league"]["round"]
+    return response
 
