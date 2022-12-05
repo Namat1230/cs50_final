b65ab3eb0f747b226d799d896694587ce2a04c66
veselyo <112834371+veselyo@users.noreply.github.com>
2022-12-05 18:11:03 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,6 @@ from flask_session import Session
 from tempfile import mkdtemp
 from werkzeug.security import check_password_hash, generate_password_hash
 
-import pprint
 
 # Define constant for the season year
 season = 2022
@@ -29,7 +28,7 @@ def after_request(response):
     return response
 
 # Routes
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("home.html")
 
@@ -42,6 +41,7 @@ def login():
 
     # User reached route via POST (as by submitting a form via POST)
     if request.method == "POST":
+
         # Connect to our database
         try:
             conn = sqlite3.connect("soccerbase.db")
@@ -62,11 +62,10 @@ def login():
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", (request.form.get("username"),))
         rows = rows.fetchall()
-        print(rows)
 
         # Ensure username exists and password is correct
         if not len(rows) or not check_password_hash(rows[0][2], request.form.get("password")):
-            return apology("invalid username and/or password", 400)
+            return apology("invalid username or password", 400)
 
         # Remember which user has logged in
         session["user_id"] = rows[0][0]
@@ -103,15 +102,13 @@ def register():
         password = request.form.get("password")
         confirmation = request.form.get("confirmation")
 
+        # Check inputs
         if not username:
             return apology("must provide username", 400)
-
         elif not password:
             return apology("must provide password", 400)
-
         elif not confirmation:
             return apology("must provide confirmation", 400)
-
         elif password != confirmation:
             return apology("password and confirmation do not match", 400)
 
@@ -127,8 +124,7 @@ def register():
 
         # Insert user into database
         else:
-            db.execute("INSERT INTO users (username, hash) VALUES (?,?)",
-                       (username, generate_password_hash(request.form.get("password"))))
+            db.execute("INSERT INTO users (username, hash) VALUES (?,?)", (username, hash))
             conn.commit()
 
         # Log In user
@@ -150,40 +146,6 @@ def logout():
     # Redirect user to login form
     return redirect("/")
 
-
-@app.route("/account")
-@login_required
-def account():
-    # Connect to our database
-    try:
-        conn = sqlite3.connect("soccerbase.db")
-        db = conn.cursor()
-
-    except sqlite3.Error as error:
-        print("Error while connecting to sqlite", error)
-        sys.exit(1)
-
-    bets = db.execute("SELECT * FROM bets WHERE user_id = ?", (13,))
-    bets = bets.fetchall()
-    if any(isinstance(el, list) for el in bets):
-        return
-    else:
-        # Info that we will display
-        info = get_fixtures_info(976458)
-        if check_bet(976458):
-            status = "Active"
-        else:
-            bet = db.execute("SELECT bet FROM bets WHERE user_id = ?", (13,))
-            bet = bet.fetchone()
-            result = get_status(976458)
-            if result[bet[0]]["winner"]:
-                status = "Bet Won"
-            else:
-                status = "Bet Lost"
-        print(status)
-        return render_template("account.html")
-
-
 @app.route("/premierleague")
 @login_required
 def premierleague():
@@ -198,17 +160,15 @@ def premierleague():
         print("Error while connecting to sqlite", error)
         sys.exit(1)
 
-    # Make standings according to user input
+    # Make standings according to user input and gather all information we need to display in our page
     standings = make_standings(convertinput(db, input), season)
     thead = standings[0]
     last = get_last_round(convertinput(db, input), season)
     next = get_next_round(convertinput(db, input), season)
 
-
     # Close SQL connection
     conn.close()
-
-    return render_template("premierleague.html", standings=standings, thead = thead, last = last, next = next)
+    return render_template("premierleague.html", standings=standings, thead=thead, last=last, next=next)
 
 @app.route("/bundesliga")
 @login_required
@@ -224,17 +184,16 @@ def bundesliga():
         print("Error while connecting to sqlite", error)
         sys.exit(1)
 
-    # Make standings according to user input
+    # Make standings according to user input and gather all information we need to display in our page
     standings = make_standings(convertinput(db, input), season)
     thead = standings[0]
     last = get_last_round(convertinput(db, input), season)
     next = get_next_round(convertinput(db, input), season)
 
-
     # Close SQL connection
     conn.close()
 
-    return render_template("bundesliga.html", standings=standings, thead = thead, last = last, next = next)
+    return render_template("bundesliga.html", standings=standings, thead=thead, last=last, next=next)
 
 @app.route("/la_liga")
 @login_required
@@ -250,17 +209,16 @@ def la_liga():
         print("Error while connecting to sqlite", error)
         sys.exit(1)
 
-    # Make standings according to user input
+    # Make standings according to user input and gather all information we need to display in our page
     standings = make_standings(convertinput(db, input), season)
     thead = standings[0]
     last = get_last_round(convertinput(db, input), season)
     next = get_next_round(convertinput(db, input), season)
 
-
     # Close SQL connection
     conn.close()
 
-    return render_template("la_liga.html", standings=standings, thead = thead, last = last, next = next)
+    return render_template("la_liga.html", standings=standings, thead=thead, last=last, next=next)
 
 @app.route("/ligue_one")
 @login_required
@@ -276,17 +234,16 @@ def ligue_one():
         print("Error while connecting to sqlite", error)
         sys.exit(1)
 
-    # Make standings according to user input
+    # Make standings according to user input and gather all information we need to display in our page
     standings = make_standings(convertinput(db, input), season)
     thead = standings[0]
     last = get_last_round(convertinput(db, input), season)
     next = get_next_round(convertinput(db, input), season)
 
-
     # Close SQL connection
     conn.close()
 
-    return render_template("ligue_one.html", standings=standings, thead = thead, last = last, next = next)
+    return render_template("ligue_one.html", standings=standings, thead=thead, last=last, next=next)
 
 @app.route("/serie_a")
 @login_required
@@ -302,17 +259,16 @@ def serie_a():
         print("Error while connecting to sqlite", error)
         sys.exit(1)
 
-    # Make standings according to user input
+    # Make standings according to user input and gather all information we need to display in our page
     standings = make_standings(convertinput(db, input), season)
     thead = standings[0]
     last = get_last_round(convertinput(db, input), season)
     next = get_next_round(convertinput(db, input), season)
 
-
     # Close SQL connection
     conn.close()
 
-    return render_template("serie_a.html", standings=standings, thead = thead, last = last, next = next)
+    return render_template("serie_a.html", standings=standings, thead=thead, last=last, next=next)
 
 @app.route("/wcbetting", methods=["GET", "POST"])
 @login_required
@@ -340,25 +296,25 @@ def worldcup():
         info = get_fixtures_info(fixtures)
 
         length = len(info)
-
         conn.close()
-
         return render_template("wcbetting.html", odds = odds, info = info, length = length, fixtures = fixtures)
 
     else:
+        # Get info form forms
         fixture = request.form.get("fixture")
         team = request.form.get("team")
         odds = request.form.get("odds")
         money = request.form.get("money")
 
+        # Check inputs
         if not fixture or not team or not odds:
             return apology("Click a Button to Bet!", 400)
-
         if not money:
             return apology("Put in Money", 400)
-
         if not money.isdigit():
             return apology("Invalid amount", 400)
+
+        # Min bet 10$
         money = int(money)
         if money < 10:
             return apology("The minimum bet amount is 10$")
@@ -373,20 +329,22 @@ def worldcup():
             sys.exit(1)
 
         # Update user balance
-        current = db.execute("SELECT cash FROM users WHERE id = ?", (session["user_id"], ))
+        current = db.execute("SELECT cash FROM users WHERE id = ?", (session["user_id"],))
         current = current.fetchone()
-        print(current)
+        current = int(current[0])
+        money = int(money)
         if current < money:
             return apology("Can't afford")
         else:
             db.execute("UPDATE users SET cash = ? WHERE id = ?", (current-money, session["user_id"]))
+            conn.commit()
 
-        # Update history
-        db.execute("INSERT INTO bets (user_id, amount, fixture, bet) VALUES (?, ?, ?, ?)", (session["user_id"]), money, fixture, team)
-
-        return redirect("/")
-
-
+        # Update bets history
+        db.execute("INSERT INTO bets (user_id, amount, fixture, bet, odds) VALUES (?, ?, ?, ?, ?)",
+                  ((session["user_id"]), money, fixture, team, odds))
+        conn.commit()
+        conn.close()
+        return redirect("/account")
 
 @app.route("/wcstandings")
 @login_required
@@ -402,12 +360,71 @@ def wcstandings():
         print("Error while connecting to sqlite", error)
         sys.exit(1)
 
-    # Make standings according to user input
+    # Make standings according to user input and gather all information we need to display in our page
     standings = make_standings(convertinput(db, input), season)
     index = list(standings.keys())
     index = index[0]
     thead = standings[index][0]
     # Close SQL connection
     conn.close()
-
     return render_template("wcstandings.html", standings = standings, thead = thead)
+
+@app.route("/account")
+@login_required
+def account():
+    # Connect to our database
+    try:
+        conn = sqlite3.connect("soccerbase.db")
+        db = conn.cursor()
+
+    except sqlite3.Error as error:
+        print("Error while connecting to sqlite", error)
+        sys.exit(1)
+
+    # Store the information for all bets of the user
+    bets = db.execute("SELECT * FROM bets WHERE user_id = ?", (session["user_id"],))
+    bets = bets.fetchall()
+
+    # Lists that we will display in html page
+    display = []
+    results = []
+    details = []
+
+    # Loop through every bet
+    for item in bets:
+
+        # Info that we will display, appennd to the list we will use
+        info = get_fixtures_info(item[2])
+        display.append(info)
+
+        # Details of the bet
+        tmp = []
+        tmp.append(item[3])
+        tmp.append(item[1])
+        tmp.append(item[6])
+        tmp.append(item[5])
+
+        # Check the status of the bet and change it accordingly; Bet is stil active
+        if check_bet(item[2]):
+            status = "Active"
+        else:
+            # Bet won, update user cash according to the odds and his bet amount
+            result = get_status(item[2])
+            if result[item[3]]["winner"]:
+                status = "Bet Won"
+                db.execute("UPDATE users SET cash = cash + ? WHERE id = ?", (int(item[1]*item[6]), session["user_id"]))
+                conn.commit()
+            else:
+                # Bet lost
+                status = "Bet Lost"
+
+        # Update lists for every bet
+        results.append(status)
+        details.append(tmp)
+
+    # Check user balance, we will display it on the website
+    balance = db.execute("SELECT cash FROM users WHERE id = ?", (session["user_id"],))
+    balance = balance.fetchone()
+    balance = balance[0]
+    length = len(results)
+    return render_template("account.html", balance=balance, display=display, results=results, length=length, details=details)
